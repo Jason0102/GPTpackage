c8dc95daeee452138ea3776367b1029bec9cec44
Jia-Hsun Lo <57864575+Jason0102@users.noreply.github.com>
2024-09-27 19:39:52 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,32 +1,38 @@
-from GPTpackages.GPTopenai import GPTopenai
-from GPTpackages.PromptTemplate import PromptTemplate
-from GPTpackages.ImageBufferMemory import ImageBufferMemory, encode_image
-from datetime import datetime
-from pathlib import Path
-import configparser
-from langchain.memory import ConversationBufferMemory
+from GPTpackage.LLMopenai import GPT, Embedding
+from GPTpackage.Buffer import ImageBuffer, TextBuffer, encode_image
+from GPTpackage.PromptTemplate import PromptTemplate
 
-API_KEY="inter your openai key"
-Model="inter model name from openai api"
+# inter your openai key
+API_KEY="your key"
 
-def showtime():
-    currentDateAndTime = datetime.now()
-    currentTime = currentDateAndTime.strftime("%H-%M-%S")
-    print(currentTime)
+# inter model name from openai api
+MODEL="gpt-4o-mini"
 
-if __name__ == '__main__':
-    path = Path('./prompts') / Path('vision_prompt.txt')
-    agent = GPTopenai(
+
+def embedding_retrieval():
+    docs = ['家庭', '籃球', '學生', '婚禮']
+    model = Embedding(
+        openai_api_key = API_KEY,
+        documents=docs
+    )
+    query = '夫妻'
+    text = model.retrieve(query, k=3)
+    print(text)
+
+def chat_gpt():
+    agent = GPT(
         openai_api_key=API_KEY, 
         model=MODEL,
-        prompt=PromptTemplate(path), 
-        text_memory=ConversationBufferMemory(memory_key="chat_history", return_messages=True, k=3),
-        img_memory=ImageBufferMemory()
+        prompt=PromptTemplate('./prompts/vision_prompt.txt'), 
+        text_memory=TextBuffer(buffer_size=3),
+        img_memory=ImageBuffer()
     )
     text_dict = {'what': '你看到了幾個杯子?他們是什麼顏色的?'}
-    path = Path('./input_pictures') / Path('IMG_8405.jpg')
-    showtime()
+    path = './input_pictures/IMG_8405.jpg'
     img_list = [encode_image(path)]
     result = agent.run(text_dict, img_list)
-    showtime()
     print(result)
+
+if __name__ == "__main__":
+    embedding_retrieval()
+    chat_gpt()
