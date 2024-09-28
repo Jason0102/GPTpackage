a66f924154f69f97466469261f27ffe4f616952b
Jia-Hsun Lo <57864575+Jason0102@users.noreply.github.com>
2024-09-28 21:45:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,11 @@
-from GPTpackage.LLMopenai import GPT, Embedding
+from GPTpackage.LLMopenai import GPT
 from GPTpackage.Buffer import ImageBuffer, TextBuffer, encode_image
 from GPTpackage.PromptTemplate import PromptTemplate
 
 # inter your openai key
 API_KEY="your key"
-
 # inter model name from openai api
 MODEL="gpt-4o-mini"
-
-
-def embedding_retrieval():
-    docs = ['家庭', '籃球', '學生', '婚禮']
-    model = Embedding(
-        openai_api_key = API_KEY,
-        documents=docs
-    )
-    query = '夫妻'
-    text = model.retrieve(query, k=3)
-    print(text)
 
 def chat_gpt():
     stm = TextBuffer(buffer_size=3)
@@ -37,6 +25,3 @@ def chat_gpt():
     stm.set(['你看到了幾個杯子?他們是什麼顏色的?', result])
     print(stm.get())
 
-if __name__ == "__main__":
-    embedding_retrieval()
-    chat_gpt()
